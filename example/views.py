4dbd420370ce95e6033d3fb7f988f244ffdc2564
xiangnan <280145668@qq.com>
2025-02-10 17:07:32 +0800

    Refactor models and views for Django ORM query demonstration
    - Update models.py with more comprehensive Django model examples
    - Replace existing models with Blog, Author, and Entry models
    - Modify views.py to use new models and demonstrate more complex ORM queries
    - Update index.html with internationalized text and minor UI improvements

--- example/views.py
+++ example/views.py
@@ -2,28 +2,26 @@ from django.shortcuts import render
 from django.db.models import Count, Sum, Avg, Min, Max, F, Q, Value, ExpressionWrapper, DecimalField, IntegerField
 from django.db.models.functions import Coalesce, Extract, Now, Concat, Cast
 from django.http import HttpResponse
-from .models import Author, Article
+from .models import *
 from django.utils.safestring import mark_safe
 import sqlparse
 import re
 import os
 
 def highlight_sql(sql):
-    # 格式化SQL
     sql = sqlparse.format(sql, reindent=True, keyword_case='upper')
-
-
     return mark_safe(sql)
 
-init_query = "Author.objects.annotate(article_count=Count('articles')).filter(article_count__gt=2)"
+init_query = '''
+Blog.objects.alias(entries=Count("entry")).annotate(
+    entries=F("entries"),
+).aggregate(Sum("entries"))
+'''
 def index(request):
     query_str = request.GET.get('q', init_query)
     queryset = eval(query_str)
     sql = str(queryset.query)
-    # 格式化并高亮SQL
     formatted_sql = highlight_sql(sql)
-
-    # 读取models.py的内容
     current_dir = os.path.dirname(__file__)
     models_path = os.path.join(current_dir, 'models.py')
     with open(models_path, 'r') as f:
@@ -34,5 +32,3 @@ def index(request):
         'sql': formatted_sql,
         'models_content': models_content
     })
-
-# Create your views here.
