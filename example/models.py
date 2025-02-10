76389e7435ffda93e1517507a13a3ffda2a18817
xiangnan <280145668@qq.com>
2025-02-10 18:12:39 +0800

    Expand Django models with Publisher, Book, and Store classes
    - Add new models to demonstrate more complex ORM relationships
    - Include Author model with age field
    - Implement ManyToMany and ForeignKey relationships between models
    - Add comprehensive model fields for Book model with various data types

--- example/models.py
+++ example/models.py
@@ -3,6 +3,7 @@ from datetime import date
 from django.db import models
 
 # https://docs.djangoproject.com/en/dev/topics/db/queries/
+# https://docs.djangoproject.com/en/dev/topics/db/aggregation/
 
 class Blog(models.Model):
     name = models.CharField(max_length=100)
@@ -15,6 +16,7 @@ class Blog(models.Model):
 class Author(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField()
+    age = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
@@ -33,3 +35,21 @@ class Entry(models.Model):
 
     def __str__(self):
         return self.headline
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=300)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=300)
+    pages = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    rating = models.FloatField()
+    authors = models.ManyToManyField(Author)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    pubdate = models.DateField()
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=300)
+    books = models.ManyToManyField(Book)
